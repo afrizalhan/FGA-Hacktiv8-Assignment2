3d506b471fc202f17515f100b89b75327fc1dd7e
Raehan Afrizal Wicaksono <afrizalraehan6@gmail.com>
2022-10-07 16:46:58 +0700

    db migrate

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	// "errors"
+	// "fmt"
+	"rest-api-go/database"
+	// "rest-api-go/models"
+	// "gorm.io/gorm"
+)
+
+func main(){
+	database.StartDB()
+}
---
+++ models/item.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	// "errors"
+	// "fmt"
+	// "time"
+
+	// "gorm.io/gorm"
+)
+
+type Item struct {
+	ItemID uint `gorm:"primary_key"`
+	ItemCode string `gorm:"not null;unique;type:varchar(191)"`
+	Descirption string `gorm:"not null;type:varchar(191)"`
+	Quantity int `gorm:"not null;type:int"`
+	OrderID uint
+}
+
+// func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
+// 	fmt.Println("Product before create()")
+
+// 	if len(p.Name) < 4 {
+// 		err = errors.New("Product name is too short")
+// 	}
+
+// 	return
+// }
