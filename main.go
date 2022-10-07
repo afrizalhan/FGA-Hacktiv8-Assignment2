b326ddaabf5c31c6f45566b38e8e9f30714b3066
Raehan Afrizal Wicaksono <afrizalraehan6@gmail.com>
2022-10-07 21:03:14 +0700

    1/4 endpoint (createOrder)

--- main.go
+++ main.go
@@ -1,13 +1,96 @@
 package main
 
-import(
+import (
+	"fmt"
+	"time"
 	// "errors"
 	// "fmt"
+	"net/http"
 	"rest-api-go/database"
-	// "rest-api-go/models"
+	"rest-api-go/models"
+
 	// "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
+	port := ":8080"
+
 	database.StartDB()
+
+	router := gin.Default()
+
+	router.POST("/orders", createOrder)
+
+	fmt.Println("server is running on port", port)
+
+	router.Run(port)
+}
+
+func createOrder(ctx *gin.Context){
+	db := database.GetDB()
+
+	var body models.Body
+	var newOrder models.Order
+	var items []models.Item
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	newOrder.CustomerName = body.CustomerName
+	newOrder.OrderedAt = time.Now()
+
+	var check bool
+
+	for _, item := range body.Items {
+		check = checkCodeExist(item.ItemCode)
+	}
+
+	if check {
+		ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("item code already exist"))
+		return
+	}
+
+	err := db.Create(&newOrder).Error
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	for _, item := range body.Items {
+		var newItem models.Item
+
+		newItem.ItemCode = item.ItemCode
+		newItem.Description = item.Description
+		newItem.Quantity = item.Quantity
+		newItem.OrderID = newOrder.OrderID
+
+		items = append(items, newItem)
+	}
+
+	err = db.Create(&items).Error
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, fmt.Errorf("failed storing item"))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "order has been recorded"})
+}
+
+func checkCodeExist(id string) bool{
+	db := database.GetDB()
+
+	var item models.Item
+
+	r := db.Where("item_code = ?", id).First(&item)
+
+	if r.RowsAffected > 0{
+		return true
+	} 
+
+	return false
 }
--- models/item.go
+++ models/item.go
@@ -11,7 +11,7 @@ import (
 type Item struct {
 	ItemID uint `gorm:"primary_key"`
 	ItemCode string `gorm:"not null;unique;type:varchar(191)"`
-	Descirption string `gorm:"not null;type:varchar(191)"`
+	Description string `gorm:"not null;type:varchar(191)"`
 	Quantity int `gorm:"not null;type:int"`
 	OrderID uint
 }
